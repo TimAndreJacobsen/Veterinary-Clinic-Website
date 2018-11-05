5daccca067423e77d0578f8f2072aeafd655c0fb
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-05 12:43:35 +0100

    Added loading of CSS and JS to functions.php
    Added custom clinic theme,
    Google font,
    Bootstrap font
    
    then queued functions to wordpress

--- app/public/wp-content/themes/clinictheme/functions.php
+++ app/public/wp-content/themes/clinictheme/functions.php
@@ -1,9 +1,23 @@
 <?php
 
-function clinic_resources(){
+function clinic_resources(){ /* function to load CSS and JavaScript */
+    /* CSS */
     wp_enqueue_style('clinic_styles', get_stylesheet_uri());
+    wp_enqueue_style('font_google_roboto', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+    /* JavaScript */
+    wp_enqueue_script('clinic_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
 }
 
-add_action('wp_enqueue_scripts', 'clinic_resources')
+function clinic_features(){
+    add_theme_support('title-tag');
+}
+
+/* Add clinic_resources() to be handled by wp */
+add_action('wp_enqueue_scripts', 'clinic_resources');
+
+/* Add title to pages */
+add_action('after_setup_theme', 'clinic_features')
 
 ?>
