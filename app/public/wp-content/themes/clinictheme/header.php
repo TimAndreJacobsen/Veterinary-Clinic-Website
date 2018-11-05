255b5ff45b068e8224f5568ac315b25261b950f6
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-05 12:48:20 +0100

    header.php now has basic navigation

--- app/public/wp-content/themes/clinictheme/header.php
+++ app/public/wp-content/themes/clinictheme/header.php
@@ -5,4 +5,26 @@
     </head>
 
     <body>
-        
+      <header class="site-header">
+        <div class="container">
+          <h1 class="clinic-logo-text float-left"><a href=" <?php echo site_url() ?> ">The <strong>Clinic</strong></a></h1>
+          <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+          <div class="site-header__menu group">
+            <nav class="main-navigation">
+              <ul>
+                <li><a href="<?php echo site_url('/about-us') ?> ">About Us</a></li>
+                <li><a href="#">Programs</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Articles</a></li>
+              </ul>
+            </nav>
+            <div class="site-header__util">
+              <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+              <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+              <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            </div>
+          </div>
+        </div>
+      </header>
+          
