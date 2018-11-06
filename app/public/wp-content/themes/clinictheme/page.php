cbdf10ada49370da39b18783c6fa26616c89036c
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-06 13:04:37 +0100

    Implemented sidebar logic for parent-child pages
    Parent and child pages will display a sidebar with navigation.
    Single pages with no child-parent relationship will not display a nav-bar.
    Cleaned up code and style

--- app/public/wp-content/themes/clinictheme/page.php
+++ app/public/wp-content/themes/clinictheme/page.php
@@ -1,50 +1,65 @@
 <?php 
-
-get_header();
-
-while(have_posts()) {
-    the_post(); ?>
+  get_header();
+  while(have_posts()) {
+      the_post(); 
+?>
     
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/cocker-and-cat-orange.jpg') ?>);"></div>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/cocker-and-cat-orange.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
-      </div>
-    </div>  
-  </div>
+        <div class="page-banner__intro">
+          <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
+        </div>
+     </div>  
+    </div>
 
   <div class="container container--narrow page-section">
 
     <!-- Breadcrumb box -->
     <?php
-    $theParentPage = wp_get_post_parent_id(get_the_ID());
+      $theParentPage = wp_get_post_parent_id(get_the_ID());
       if ($theParentPage) { ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
           <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentPage); ?> "><i class="fa fa-home" aria-hidden="true"></i> <?php echo get_the_title($theParentPage); ?> </a> <span class="metabox__main"> <?php echo the_title(); ?> </span></p>
         </div>
-      <?php }
+    <?php }
     ?>
 
+    <!-- Right hand sidebar box in child pages -->
+    <?php 
+      /* Check if the page is a parent page */
+      $isParentPage = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+      if ($theParentPage or $isParentPage) {
+    ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentPage); ?> "> <?php echo get_the_title($theParentPage) ?> </a></h2>
+        <ul class="min-list">
+        <?php
+          if ($theParentPage){
+            $findChildren = $theParentPage;
+          } else {
+            $findChildren = get_the_ID();
+          }
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildren
+          ));
+        ?>
+        </ul>
+      </div>
+      <?php } ?>
 
-    <!-- COMMENTED OUT while it waits to be implemented 
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
-    -->
+    <!-- end of sidebar box in child pages -->
+
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
 
-  </div>
+  </div> <!-- end of narrow-page-section div -->
 
 <?php }
-
-get_footer();
-
+  get_footer();
 ?>
