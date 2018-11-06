7831e2eea4802aac151472793ff826989e14cfda
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-06 12:19:11 +0100

    Implemented breadcrumb/metabox for parent-child pages
    Breadcrumb box now displays on children pages.
    On children pages it will give a link back to the parent page and output what the current child page is.

--- app/public/wp-content/themes/clinictheme/page.php
+++ app/public/wp-content/themes/clinictheme/page.php
@@ -17,10 +17,18 @@ while(have_posts()) {
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
-    <!-- COMMENTED OUT while it waits to be implemented
+    <!-- Breadcrumb box -->
+    <?php
+    $theParentPage = wp_get_post_parent_id(get_the_ID());
+      if ($theParentPage) { ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+          <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentPage); ?> "><i class="fa fa-home" aria-hidden="true"></i> <?php echo get_the_title($theParentPage); ?> </a> <span class="metabox__main"> <?php echo the_title(); ?> </span></p>
+        </div>
+      <?php }
+    ?>
+
+
+    <!-- COMMENTED OUT while it waits to be implemented 
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
