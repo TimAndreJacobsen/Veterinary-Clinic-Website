52c8c964f32fe138e26bb8914f651cd9459287de
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-06 14:21:27 +0100

    added meta and language tags to <head>

--- app/public/wp-content/themes/clinictheme/header.php
+++ app/public/wp-content/themes/clinictheme/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?> >
     <head>
+        <meta charset="<?php bloginfo('charset'); ?> ">
+        <meta name="viewport" content="width=device-width", initial-scale=1>
         <?php wp_head(); ?>
     </head>
 
