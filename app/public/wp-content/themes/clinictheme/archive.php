87bb88b82c3ce46f19853dc853b00e3d9ae469f9
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-07 15:46:15 +0100

    basic template complete
    now has banner, proper post spacing, CSS classes, posts with working links

--- app/public/wp-content/themes/clinictheme/archive.php
+++ app/public/wp-content/themes/clinictheme/archive.php
@@ -12,9 +12,28 @@
     </div>
   </div>
 </div>
+<br><br>
+<div class="container container--narrow page section">
+<?php
+  while(have_posts()) {
+      the_post(); ?>
+     <div class="post-item">
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?> "><?php the_title(); ?> </a></h2>
 
+      <div class="metabox">
+        <p>Posted by <?php the_author_posts_link(); ?> on <?php echo get_the_date('F j, Y'); ?> in <?php echo get_the_category_list(', '); ?> </p>
+      </div>
 
+      <div class="generic-content">
+        <?php the_excerpt(); ?>
+        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Read More</a></p>
+      </div>
+    </div>  
+  <?php }
+    echo paginate_links();
+?>
+</div>
 
-<?php 
-    get_footer();
+<?php
+  get_footer();
 ?>
