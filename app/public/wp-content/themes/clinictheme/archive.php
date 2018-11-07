7e4111d27d4982b92ff958ac40292009abd503fd
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-07 16:08:24 +0100

    reimplemented if statements and made some visual changes
    not enough fine grain control with wordpress function the_archive_title();

--- app/public/wp-content/themes/clinictheme/archive.php
+++ app/public/wp-content/themes/clinictheme/archive.php
@@ -7,8 +7,14 @@
         url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
   </div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"> <?php the_archive_title(); ?>
-    <div class="page-banner__intro">
+    <h1 class="page-banner__title">
+    <?php if(is_category()) {
+        single_cat_title();
+    } if (is_author()) {
+        echo "Posts By:"; ?>
+    <?php }
+    ?></h1>
+    <div class="page-banner__intro"> <?php if(is_author()) {the_author(); } ?>
     </div>
   </div>
 </div>
