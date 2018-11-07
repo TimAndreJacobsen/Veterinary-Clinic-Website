3f48f79ce23d944cff0cb3eb8fa9c1e7ebb6b42f
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-07 10:19:06 +0100

    placed code from index.php into front-page.php

---
+++ app/public/wp-content/themes/clinictheme/front-page.php
@@ -0,0 +1,90 @@
+<?php 
+
+get_header(); ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/dogs-whitebg.jpg') ?>);"></div>
+    <div class="page-banner__content container t-center c-white">
+      <h1 class="headline headline--large">Welcome!</h1>
+      <h2 class="headline headline--medium">To the Clinic</h2>
+      <h3 class="headline headline--small">Browse our services and book an appointment</h3>
+      <a href="#" class="btn btn--large btn--blue">Book an Appointment</a>
+    </div>
+  </div>
+
+  <div class="full-width-split group">
+    <div class="full-width-split__one">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+        
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="#">
+            <span class="event-summary__month">Mar</span>
+            <span class="event-summary__day">25</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Dog socializing and playtime event</a></h5>
+            <p>Bring your pet this Tuesday for an playful experience in our yard. <a href="#" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+        
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+
+      </div>
+    </div>
+    <div class="full-width-split__two">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">Articles by our Staff</h2>
+
+        <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="#">
+            <span class="event-summary__month">Jan</span>
+            <span class="event-summary__day">20</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best Clinic</a></h5>
+            <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+        <div class="event-summary">
+          <a class="event-summary__date event-summary__date--beige t-center" href="#">
+            <span class="event-summary__month">Feb</span>
+            <span class="event-summary__day">04</span>  
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="#">Vets in the National Spotlight</a></h5>
+            <p>Two of our Vets have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
+          </div>
+        </div>
+        
+        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Posted Articles</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="hero-slider">
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Accessible by public transport</h2>
+        <p class="t-center">Check out which public transport options are avaliable</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/brew-and-beans.jpg') ?>);">
+    <div class="hero-slider__interior container">
+      <div class="hero-slider__overlay">
+        <h2 class="headline headline--medium t-center">Free Coffee</h2>
+        <p class="t-center">We have fresh coffee for those in need.</p>
+        <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+
+get_footer();
+
+?>
