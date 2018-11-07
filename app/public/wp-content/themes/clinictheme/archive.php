d96105f70f1f55c9953c4ca97e3ffaf04cc1f824
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-07 15:27:35 +0100

    basic archives.php template for handling view by author, category, etc
    basic skeleton

---
+++ app/public/wp-content/themes/clinictheme/archive.php
@@ -0,0 +1,20 @@
+<?php 
+    get_header();
+?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: 
+        url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
+  </div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Archives</h1>
+    <div class="page-banner__intro">
+    </div>
+  </div>
+</div>
+
+
+
+<?php 
+    get_footer();
+?>
