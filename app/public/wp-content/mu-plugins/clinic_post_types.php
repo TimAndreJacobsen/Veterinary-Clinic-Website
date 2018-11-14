fa4c20088a523c5e37fa7931c436796e224b0048
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-14 10:53:19 +0100

    Adding new post_type Events

---
+++ app/public/wp-content/mu-plugins/clinic_post_types.php
@@ -0,0 +1,19 @@
+<?php
+
+function clinic_post_types(){
+    register_post_type('event', array(
+        'public' => true,
+        'menu_icon' => 'dashicons-layout',
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add new Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        )
+    ));
+}
+
+add_action('init', 'clinic_post_types');
+
+?>
