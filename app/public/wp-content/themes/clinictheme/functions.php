5299ddf49298a34bd2ec2952cada94822260dfc8
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-15 14:21:08 +0100

    added hooks onto wp queries for events. Now displays upcoming events
    No longer shows already completed events.

--- app/public/wp-content/themes/clinictheme/functions.php
+++ app/public/wp-content/themes/clinictheme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function clinic_resources(){ /* function to load CSS and JavaScript */
+function clinic_resources()
+{ /* function to load CSS and JavaScript */
     /* CSS */
     wp_enqueue_style('clinic_styles', get_stylesheet_uri());
     wp_enqueue_style('font_google_roboto', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -9,7 +10,8 @@ function clinic_resources(){ /* function to load CSS and JavaScript */
     wp_enqueue_script('clinic_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
 }
 
-function clinic_features(){
+function clinic_features()
+{
     /* Add title to pages */
     add_theme_support('title-tag');
     /* Add header menu to wp-admin */
@@ -18,10 +20,29 @@ function clinic_features(){
     register_nav_menu('footer_menu_location_right', 'Footer Menu Location Right');
 }
 
+function event_archive_queries_custom($query){
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => date('Ymd'),/* loads todays date for use in meta_query */
+              'type' => 'numeric'
+            )));
+    }
+}
+
 /* Add CSS and JS to be handled by wp */
 add_action('wp_enqueue_scripts', 'clinic_resources');
 
 /* function to load CSS and JavaScript */
 add_action('after_setup_theme', 'clinic_features');
+
+/*  */
+add_action('pre_get_posts', 'event_archive_queries_custom')
+
 
 ?>
