da0692a50588ae672b7403726c2ab29c2ffb8348
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-15 11:39:56 +0100

    added archive and new slug to post_type Event

--- app/public/wp-content/mu-plugins/clinic_post_types.php
+++ app/public/wp-content/mu-plugins/clinic_post_types.php
@@ -4,13 +4,15 @@ function clinic_post_types()
 {
     register_post_type('event', array(
         'public' => true,
+        'has_archive' => true,
         'menu_icon' => 'dashicons-layout',
+        'rewrite' => array('slug' => 'events'),
         'labels' => array(
             'name' => 'Events',
             'add_new_item' => 'Add new Event',
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
-            'singular_name' => 'Event'
+            'singular_name' => 'Event',
         )
     ));
 }
