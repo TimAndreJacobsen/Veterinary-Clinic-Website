9e0a66cf67b1b8877929faefe2ec119641ebc398
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-15 14:22:56 +0100

    archive of events now displays the date of the event in the date-icon

--- app/public/wp-content/themes/clinictheme/archive-event.php
+++ app/public/wp-content/themes/clinictheme/archive-event.php
@@ -8,26 +8,31 @@
   </div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">All Events</h1>
-    <div class="page-banner__intro">
+    <div class="page-banner__intro">Past events hosted by the Clinic
     </div>
   </div>
 </div>
 <br><br>
 <div class="container container--narrow page section">
-<?php
+  <?php
   while (have_posts()) {
-    the_post(); ?>
-        <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php echo get_the_date('M'); ?></span>
-            <span class="event-summary__day"><?php echo get_the_date('j'); ?></span>  
-        </a>
-        <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 30); ?><a href="<?php the_permalink(); ?>" class="nu gray">
-            <br>See more</a></p>
-        </div>
-        </div>
+      the_post(); 
+      $eventDate = new DateTime(get_field('event_date', false, false)); ?>
+  <div class="event-summary">
+    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+      <span class="event-summary__month">
+        <?php echo $eventDate->format('M'); ?></span>
+      <span class="event-summary__day">
+        <?php echo $eventDate->format('d'); ?></span>
+    </a>
+    <div class="event-summary__content">
+      <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
+          <?php the_title(); ?></a></h5>
+      <p>
+        <?php echo wp_trim_words(get_the_content(), 30); ?><a href="<?php the_permalink(); ?>" class="nu gray">
+          <br>See more</a></p>
+    </div>
+  </div>
   <?php
   }
     echo paginate_links();
