52b752500732069c407304256fa7238030679122
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-16 12:59:13 +0100

    replaced placeholder text

--- app/public/wp-content/themes/clinictheme/single-treatment.php
+++ app/public/wp-content/themes/clinictheme/single-treatment.php
@@ -7,18 +7,16 @@ while (have_posts()) {
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/bassets.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">
-      <?php the_title(); ?>
-    </h1>
+    <h1 class="page-banner__title">Treatment</h1>
     <div class="page-banner__intro">
-      <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
+      <p><?php the_title(); ?></p>
     </div>
   </div>
 </div>
 <div class="container container--narrow page-section">
   <div class="metabox metabox--position-up metabox--with-home-link">
     <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('treatment'); ?> ">
-        <i class="fa fa-home" aria-hidden="true"></i> All Treatments</a>
+        <i class="fa fa-home" aria-hidden="true"></i> Treatments</a>
       <span class="metabox__main">
         <?php the_title(); ?> </span></p>
   </div>
