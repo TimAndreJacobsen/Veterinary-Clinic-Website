b232a58a402c217da2b9337f7c6053fdb2da6a2f
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-16 12:57:29 +0100

    archive page for the different buildings at location

---
+++ app/public/wp-content/themes/clinictheme/archive-locale.php
@@ -0,0 +1,33 @@
+<?php
+    get_header();
+?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: 
+        url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
+  </div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Visit us at our location</h1>
+    <div class="page-banner__intro">Everything your pets need in one place
+    </div>
+  </div>
+</div>
+<br><br>
+
+<div class="container container--narrow page section">
+  <ul class="link-list min-list">
+    <?php
+  while (have_posts()) {
+      the_post(); ?>
+    <li><a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?></a></li>
+    <?php
+  }
+    echo paginate_links();
+?>
+  </ul>
+</div>
+
+<?php
+  get_footer();
+?>
