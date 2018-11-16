63c95af22edce8caae629abe87d272673e05fb1b
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-16 12:57:52 +0100

    made some style changes

--- app/public/wp-content/themes/clinictheme/archive-treatment.php
+++ app/public/wp-content/themes/clinictheme/archive-treatment.php
@@ -7,26 +7,25 @@
         url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
   </div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">All Treatments</h1>
-    <div class="page-banner__intro">A excerpt of our avaliable treatments
+    <h1 class="page-banner__title">Treatment</h1>
+    <div class="page-banner__intro">A excerpt of our avaliable services
     </div>
   </div>
 </div>
 <br><br>
+
 <div class="container container--narrow page section">
   <ul class="link-list min-list">
-  <?php
+    <?php
   while (have_posts()) {
       the_post(); ?>
-      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-  
-  <?php
+    <li><a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?></a></li>
+    <?php
   }
     echo paginate_links();
 ?>
   </ul>
-
-
 </div>
 
 <?php
