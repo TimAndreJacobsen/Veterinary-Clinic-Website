806e4671c350fe8db00b526accad88cd171141b6
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-11-16 10:57:46 +0100

    added archive page to handle displaying all treatments

---
+++ app/public/wp-content/themes/clinictheme/archive-treatment.php
@@ -0,0 +1,34 @@
+<?php
+    get_header();
+?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: 
+        url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
+  </div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">All Treatments</h1>
+    <div class="page-banner__intro">A excerpt of our avaliable treatments
+    </div>
+  </div>
+</div>
+<br><br>
+<div class="container container--narrow page section">
+  <ul class="link-list min-list">
+  <?php
+  while (have_posts()) {
+      the_post(); ?>
+      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+  
+  <?php
+  }
+    echo paginate_links();
+?>
+  </ul>
+
+
+</div>
+
+<?php
+  get_footer();
+?>
