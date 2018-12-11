a6f519abbd889ad088f01dda27bdcde13a449a0c
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-11 11:51:43 +0100

    single-event now has a Locale relationship
    If an event is tied to a locale, the a link to the locale(s) will be given below the event_content.
    If no Locale is related, an if() statement will catch it and prevent the code from being run.
    Added more comments to the code.

--- app/public/wp-content/themes/clinictheme/single-event.php
+++ app/public/wp-content/themes/clinictheme/single-event.php
@@ -15,25 +15,34 @@ while (have_posts()) {
     </div>
   </div>
 </div>
+<?php /* Metabox on banner */ ?>
 <div class="container container--narrow page-section">
   <div class="metabox metabox--position-up metabox--with-home-link">
     <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?> ">
         <i class="fa fa-home" aria-hidden="true"></i> Events Page</a>
       <span class="metabox__main">
-        <?php the_title(); ?> </span></p>
+        <?php the_title(); /* title in metabox */ ?> </span></p>
   </div>
   <div class="generic_content">
-    <?php the_content(); ?>
+    <?php the_content(); /* displaying post_content below banner */ ?>
   </div>
   
   <?php
-    $eventLocale = get_field('related_locales');
-    print_r($eventLocale)
-  ?>
+  /* Handles displaying related locales for events with a locale relationship */
+    $relatedLocales = get_field('related_locales');
+    if($relatedLocales){ /* Checks if event has related locales, before displaying */
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">Related Locales</h2>';
+      echo '<ul class="link-list min-list">';
+      foreach($relatedLocales as $locale){ ?>
+        <li><a href="<?php echo get_the_permalink($locale);?>"> <?php echo get_the_title($locale); ?></a></li>
+      <?php }
+      echo '</ul>';
+    }
+    ?>
 
-</div>
-<?php
-}
+  </div>
+  <?php }
 
 get_footer();
 
