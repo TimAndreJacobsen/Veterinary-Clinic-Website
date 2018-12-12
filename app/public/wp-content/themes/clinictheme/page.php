853293871145d4ba3c1b7b021e56c5964e55e914
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-12 16:01:11 +0100

    refactored out duplicated banner code

--- app/public/wp-content/themes/clinictheme/page.php
+++ app/public/wp-content/themes/clinictheme/page.php
@@ -1,19 +1,9 @@
 <?php
   get_header();
   while (have_posts()) {
-      the_post(); ?>
-
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/cocker-and-cat-orange.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">
-      <?php the_title(); ?>
-    </h1>
-    <div class="page-banner__intro">
-      <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
-    </div>
-  </div>
-</div>
+      the_post(); 
+      page_banner(array());
+      ?>
 
 <div class="container container--narrow page-section">
 
