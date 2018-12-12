e8b59a0aa7a7af67777634f6b5c5700f3210f848
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-12 15:00:32 +0100

    added dynamic banners selectable from admin bar
    Banner image and subtitle is customizable from admin panel.
    fixed some issues with styling and indentation

--- app/public/wp-content/themes/clinictheme/single-employee.php
+++ app/public/wp-content/themes/clinictheme/single-employee.php
@@ -4,47 +4,45 @@ get_header();
 
 while (have_posts()) {
     the_post(); ?>
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/bassets.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">
-      <?php the_title(); ?>
-    </h1>
-    <div class="page-banner__intro">
-      <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
-    </div>
-  </div>
-</div>
-
-<div class="container container--narrow page-section">
-
-  <div class="generic_content">
-    <div class="row group"> 
-      <div class="one-third"> <?php /* Employee thumbnail */
-      the_post_thumbnail('employee-portrait'); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php $bannerImage = get_field('page_banner_background_image'); echo $bannerImage['url'] ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+          <h1 class="page-banner__title"> <?php the_title(); ?></h1>
+          <div class="page-banner__intro">
+            <p><?php the_field('page_banner_subtitle') ?></p>
+          </div>
+        </div>
       </div>
-      
-      <div class="two-thirds"><?php /* Employee Bio */ 
-      the_content(); ?>
-      </div>
-    </div>
-  </div>
+
+      <div class="container container--narrow page-section">
+
+        <div class="generic_content">
+          <div class="row group"> 
+            <div class="one-third"> <?php /* Employee thumbnail */
+              the_post_thumbnail('employee-portrait'); ?>
+            </div>
+            
+            <div class="two-thirds"><?php /* Employee Bio */ 
+              the_content(); ?>
+            </div>
+          </div>
+        </div>
   
-  <?php /* Handles displaying related locales for events with a locale relationship */
-    $relatedLocales = get_field('related_locales');
-    if($relatedLocales){ /* Checks if event has related locales, before displaying */
-      echo '<hr class="section-break">';
-      echo '<h2 class="headline headline--medium">Can be found around</h2>';
-      echo '<ul class="link-list min-list">';
-      foreach($relatedLocales as $locale){ ?>
-        <li><a href="<?php echo get_the_permalink($locale);?>"> <?php echo get_the_title($locale); ?></a></li>
-      <?php }
-      echo '</ul>';
-    }
-    ?>
+        <?php /* Handles displaying related locales for events with a locale relationship */
+        $relatedLocales = get_field('related_locales');
+        if($relatedLocales){ /* Checks if event has related locales, before displaying */
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Can be found around</h2>';
+        echo '<ul class="link-list min-list">';
+        foreach($relatedLocales as $locale){ ?>
+            <li><a href="<?php echo get_the_permalink($locale);?>"> <?php echo get_the_title($locale); ?></a></li>
+        <?php }
+        echo '</ul>';
+        }
+        ?>
 
   </div>
-  <?php }
+<?php }
 
 get_footer();
 
