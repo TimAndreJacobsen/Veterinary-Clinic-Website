c30d5d5d7e74a2b88a6034b3159de80dd8458e66
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-12 09:33:55 +0100

    page template for single employee

---
+++ app/public/wp-content/themes/clinictheme/single-employee.php
@@ -0,0 +1,42 @@
+<?php
+
+get_header();
+
+while (have_posts()) {
+    the_post(); ?>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/bassets.jpg') ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">
+      <?php the_title(); ?>
+    </h1>
+    <div class="page-banner__intro">
+      <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
+    </div>
+  </div>
+</div>
+
+<div class="container container--narrow page-section">
+  <div class="generic_content">
+    <?php the_content(); /* displaying post_content below banner */ ?>
+  </div>
+  
+  <?php /* Handles displaying related locales for events with a locale relationship */
+    $relatedLocales = get_field('related_locales');
+    if($relatedLocales){ /* Checks if event has related locales, before displaying */
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">Works at</h2>';
+      echo '<ul class="link-list min-list">';
+      foreach($relatedLocales as $locale){ ?>
+        <li><a href="<?php echo get_the_permalink($locale);?>"> <?php echo get_the_title($locale); ?></a></li>
+      <?php }
+      echo '</ul>';
+    }
+    ?>
+
+  </div>
+  <?php }
+
+get_footer();
+
+?>
