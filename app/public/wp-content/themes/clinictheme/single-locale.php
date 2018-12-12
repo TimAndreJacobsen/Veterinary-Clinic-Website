29d8c6cd304f70253cad94eeef3d029b72791501
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-12 14:23:24 +0100

    optimization: changed img src to cropped version for employee-thumbnail

--- app/public/wp-content/themes/clinictheme/single-locale.php
+++ app/public/wp-content/themes/clinictheme/single-locale.php
@@ -51,7 +51,7 @@ while (have_posts()) {
         $localeEmployees->the_post(); ?>
         <li class="employee-card__list-item">
           <a class="employee-card" href="<?php the_permalink(); ?>">
-            <img class="employee-card__image" src="<?php the_post_thumbnail_url(); ?>">
+            <img class="employee-card__image" src="<?php the_post_thumbnail_url($size="employee-landscape"); ?>">
             <span class="employee-card__name"><?php the_title(); ?></span>
           </a>
         </li>
