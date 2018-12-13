1e5a2c5adac3e3b44e7eeba10bcaab7a0e8ec52a
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-13 12:35:43 +0100

    archive-event.php now uses page_banner()

--- app/public/wp-content/themes/clinictheme/archive-event.php
+++ app/public/wp-content/themes/clinictheme/archive-event.php
@@ -1,17 +1,11 @@
 <?php
     get_header();
+    page_banner(array(
+      'title' => 'All Events',
+      'subtitle' => 'See upcoming or previous events'
+    ));
 ?>
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: 
-        url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
-  </div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">All Events</h1>
-    <div class="page-banner__intro">Past events hosted by the Clinic
-    </div>
-  </div>
-</div>
 <br><br>
 <div class="container container--narrow page section">
   <?php
