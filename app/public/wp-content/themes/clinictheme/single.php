bc8feab4420c5a46e5db16554522272a3e2b067d
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-14 10:51:26 +0100

    refactored to use page_banner() function
    hardcoded background banner image for every single blog/article posts.

--- app/public/wp-content/themes/clinictheme/single.php
+++ app/public/wp-content/themes/clinictheme/single.php
@@ -1,20 +1,17 @@
 <?php
-
 get_header();
 
+/*
+ * Template for single Article/blog post
+ */
+
 while (have_posts()) {
-    the_post(); ?>
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/bassets.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">
-      <?php the_title(); ?>
-    </h1>
-    <div class="page-banner__intro">
-      <p>DONT FORGET TO REPLACE ME !#!#!#!#!#!#!#!#!#!</p>
-    </div>
-  </div>
-</div>
+    the_post(); 
+    page_banner(array(
+      'image' => get_theme_file_uri('/images/cocker-and-cat-orange.jpg')
+    ));
+    ?>
+
 <div class="container container--narrow page-section">
   <div class="metabox metabox--position-up metabox--with-home-link">
     <p><a class="metabox__blog-home-link" href="<?php echo site_url('/articles'); ?> ">
