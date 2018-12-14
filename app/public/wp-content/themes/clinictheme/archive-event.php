f46a3a015d4bb069a4d398f98ab6f537b678290a
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-14 11:28:50 +0100

    all events page now uses template-parts instead of duplicate code

--- app/public/wp-content/themes/clinictheme/archive-event.php
+++ app/public/wp-content/themes/clinictheme/archive-event.php
@@ -3,34 +3,16 @@
     page_banner(array(
       'title' => 'All Events',
       'subtitle' => 'See upcoming or previous events'
-    ));
-?>
+    )); ?>
 
 <br><br>
 <div class="container container--narrow page section">
   <?php
   while (have_posts()) {
       the_post(); 
-      $eventDate = new DateTime(get_field('event_date', false, false)); ?>
-  <div class="event-summary">
-    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-      <span class="event-summary__month">
-        <?php echo $eventDate->format('M'); ?></span>
-      <span class="event-summary__day">
-        <?php echo $eventDate->format('d'); ?></span>
-    </a>
-    <div class="event-summary__content">
-      <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
-          <?php the_title(); ?></a></h5>
-      <p>
-        <?php echo wp_trim_words(get_the_content(), 30); ?><a href="<?php the_permalink(); ?>" class="nu gray">
-      </p>
-    </div>
-  </div>
-  <?php
+      get_template_part('template-parts/events');
   }
-    echo paginate_links();
-?>
+  echo paginate_links(); ?>
 
   <hr class="section-break">
   <p>Want to browse our <a href="<?php echo site_url('/past-events') ?>">past events?</p>
