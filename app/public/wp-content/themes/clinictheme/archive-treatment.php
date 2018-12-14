dbaeb48682129dd47b5f3404d60461927e36c5eb
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-14 09:52:03 +0100

    refactored out page_banner logic from individual pages

--- app/public/wp-content/themes/clinictheme/archive-treatment.php
+++ app/public/wp-content/themes/clinictheme/archive-treatment.php
@@ -1,17 +1,11 @@
 <?php
     get_header();
+    page_banner(array(
+      'title' => 'Treatments',
+      'subtitle' => 'a subset of avaliable treatments and pricing examples'
+    ));
 ?>
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: 
-        url(<?php echo get_theme_file_uri('/images/5_dogs.jpg') ?> );">
-  </div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Treatment</h1>
-    <div class="page-banner__intro">A excerpt of our avaliable services
-    </div>
-  </div>
-</div>
 <br><br>
 
 <div class="container container--narrow page section">
