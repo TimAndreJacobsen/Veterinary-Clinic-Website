9a156dacd046503c12fb9be6ea7cba071aafc1e1
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-14 11:29:50 +0100

    single locales now use template-parts instead of duplicate code

--- app/public/wp-content/themes/clinictheme/single-locale.php
+++ app/public/wp-content/themes/clinictheme/single-locale.php
@@ -80,31 +80,11 @@ while (have_posts()) {
     <?php 
     while ($localeEvents->have_posts()) {
         $localeEvents->the_post();
-        $eventDate = new DateTime(get_field('event_date', false, false)); ?>
-        <div class="event-summary">
-          <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-          <span class="event-summary__month">
-            <?php echo $eventDate->format('M'); ?></span>
-          <span class="event-summary__day">
-            <?php echo $eventDate->format('d'); ?></span>
-          </a>
-          <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?></a></h5>
-            <p> <?php 
-            if (has_excerpt()) {
-              echo get_the_excerpt(); ?>
-              <?php
-            } else {
-              echo wp_trim_words(get_the_content(), 30);
-            } ?>
-            <a href="<?php the_permalink(); ?>" class="nu c-blue">
-          </div>
-        </div>
-    <?php }
+        get_template_part('template-parts/events');
+    }
   } wp_reset_postdata(); ?>
     
-</div> <?php /* end of main content centered div. class="container container--narrow page-section" */ ?>
+</div>
 <?php
 }
 
