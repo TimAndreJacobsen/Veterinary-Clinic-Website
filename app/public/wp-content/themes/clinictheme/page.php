5187d36b06523d47956d42e6228e15b72dc15309
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-15 10:10:18 +0100

    updated code style and indentation

--- app/public/wp-content/themes/clinictheme/page.php
+++ app/public/wp-content/themes/clinictheme/page.php
@@ -8,45 +8,41 @@
 <div class="container container--narrow page-section">
 
   <?php /* Breadcrumb box / metabox */
-      $theParentPage = wp_get_post_parent_id(get_the_ID());
-      if ($theParentPage) {
-          ?>
-  <div class="metabox metabox--position-up metabox--with-home-link">
-    <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentPage); ?> ">
-        <i class="fa fa-home" aria-hidden="true"></i>
-        <?php echo get_the_title($theParentPage); ?></a>
+  $theParentPage = wp_get_post_parent_id(get_the_ID());
+  if ($theParentPage) { ?>
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentPage); ?> ">
+      <i class="fa fa-home" aria-hidden="true"></i>
+      <?php echo get_the_title($theParentPage); ?></a>
       <span class="metabox__main">
-        <?php echo the_title(); ?> </span></p>
-  </div>
-  <?php
-      } ?>
+      <?php echo the_title(); ?> </span></p>
+    </div> 
+  <?php } ?>
 
   <?php /* Right hand sidebar box in child pages */
-      /* Check if the page is a parent page */
-      $isParentPage = get_pages(array(
-        'child_of' => get_the_ID()
-      ));
-      if ($theParentPage or $isParentPage) {
-          ?>
-  <div class="page-links">
-    <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentPage); ?> ">
-        <?php echo get_the_title($theParentPage) ?> </a></h2>
-    <ul class="min-list">
+
+  $isParentPage = get_pages(array(
+    'child_of' => get_the_ID()
+  ));
+  if ($theParentPage or $isParentPage) { /* Checks if the page is a parent page */ ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentPage); ?> ">
+      <?php echo get_the_title($theParentPage) ?> </a></h2>
+      <ul class="min-list">
       <?php
-          if ($theParentPage) {
-              $findChildren = $theParentPage;
-          } else {
-              $findChildren = get_the_ID();
-          }
-          wp_list_pages(array(
-            'title_li' => null,
-            'child_of' => $findChildren,
-            'sort_column' => 'menu_order'
-          )); ?>
-    </ul>
-  </div>
-  <?php
-      } /* end of sidebar box in child pages */ ?>
+      if ($theParentPage) {
+          $findChildren = $theParentPage;
+      } else {
+          $findChildren = get_the_ID();
+      }
+      wp_list_pages(array(
+        'title_li' => null,
+        'child_of' => $findChildren,
+        'sort_column' => 'menu_order'
+      )); ?>
+      </ul>
+    </div>
+  <?php } /* end of sidebar box in child pages */ ?>
 
   <div class="generic-content">
     <?php the_content(); ?>
