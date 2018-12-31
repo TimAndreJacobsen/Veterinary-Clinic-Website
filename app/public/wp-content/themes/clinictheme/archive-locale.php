78cba8786ad5510574db2925c34a2628e2d85a96
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-31 13:07:28 +0100

    added infobox for locale map markers

--- app/public/wp-content/themes/clinictheme/archive-locale.php
+++ app/public/wp-content/themes/clinictheme/archive-locale.php
@@ -18,6 +18,8 @@
       <div class="marker" 
         data-lat="<?php echo $map_location['lat'] ?>" 
         data-lng="<?php echo $map_location['lng'] ?>">
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <?php echo $map_location['address']; ?>
       </div>
     <?php }
     echo paginate_links(); ?>
