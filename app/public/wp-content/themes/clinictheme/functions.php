36e8932cb4c8336aea3e726f2aac5906a839e032
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-31 12:08:53 +0100

    code cleanup and added comments

--- app/public/wp-content/themes/clinictheme/functions.php
+++ app/public/wp-content/themes/clinictheme/functions.php
@@ -1,16 +1,55 @@
 <?php
 
 /*
+ * page_banner() handles banner area of a page.
+ * If no $args are passed, the function will use default values.
+ * 
+ * @args: associative array(dictionary)
+ * @param: title, subtitle, banner_image
+ * @returns: HTML and CSS snippet for banner area. 
+ * dependencies: Advanced Custom Fields plugin for get_field() function
+ */
+function page_banner($args = NULL){
+    if (!$args['title']){
+        $args['title'] = get_the_title();
+    }
+    if (!$args['subtitle']){
+        /* Grab subtitle from WP-admin area */
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+    if (!$args['image']){
+        if (get_field('page_banner_background_image')){
+            /* Grab banner image from WP-admin area */
+            $args['image'] = get_field('page_banner_background_image')['sizes']['page-banner'];
+        } else {
+            /* Default banner-image if all other options fail */
+            $args['image'] = get_theme_file_uri('/images/dogs-whitebg.jpg');
+        }
+    } /* HTML/CSS snippet returned to function caller */?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php 
+            echo $args['image']; ?>);">
+        </div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"> <?php echo $args['title']; ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle']; ?></p>
+            </div>
+        </div>
+    </div> <?php
+}
+
+/*
  * function to load CSS and JavaScript 
  */
 function clinic_resources(){
+    /* JavaScript */
     wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDzoEZVm8qLGy6Pog5Ob-xfh3Cv5YgwgrM', NULL, '1.0', true);
+    wp_enqueue_script('clinic_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     /* CSS */
-    wp_enqueue_style('clinic_styles', get_stylesheet_uri());
     wp_enqueue_style('font_google_roboto', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    /* JavaScript */
-    wp_enqueue_script('clinic_js', get_theme_file_uri('/js/scripts-bundled.js'), null, microtime(), true);
+    wp_enqueue_style('clinic_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
 /*
@@ -25,7 +64,6 @@ function clinic_features(){
     add_image_size('employee-landscape', 400, 260, true);
     add_image_size('employee-portrait', 480, 650, true);
     add_image_size('page-banner', 1500, 350, true);
-
     /* Add header menu to wp-admin */
     register_nav_menu('header_menu_location', 'Header Menu Location');
     register_nav_menu('footer_menu_location_left', 'Footer Menu Location Left');
@@ -65,63 +103,24 @@ function clinic_custom_queries($query){
     }
 }
 
-/*
- * page_banner() handles banner area of a page.
- * If no $args are passed, the function will grab default values.
- * 
- * @args: associative array(dictionary)
- * @param: title, subtitle, banner_image
- * @returns: HTML and CSS snippet for banner area. 
- * 
- * dependencies: Advanced Custom Fields plugin for get_field() function
- */
-function page_banner($args = NULL){
-    if (!$args['title']){
-        $args['title'] = get_the_title();
-    }
-    if (!$args['subtitle']){
-        /* Grab subtitle from WP-admin area */
-        $args['subtitle'] = get_field('page_banner_subtitle');
-    }
-    if (!$args['image']){
-        if (get_field('page_banner_background_image')){
-            /* Grab banner image from WP-admin area */
-            $args['image'] = get_field('page_banner_background_image')['sizes']['page-banner'];
-        } else {
-            /* Default banner-image if all other options fail */
-            $args['image'] = get_theme_file_uri('/images/dogs-whitebg.jpg');
-        }
-    } /* HTML/CSS snippet returned to function caller */?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php 
-            echo $args['image']; ?>);">
-        </div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"> <?php echo $args['title']; ?></h1>
-            <div class="page-banner__intro">
-                <p><?php echo $args['subtitle']; ?></p>
-            </div>
-        </div>
-    </div> <?php
-}
-
 /* Google Maps API-key */
 function acf_google_maps_api_key($api){
     $api['key'] = 'AIzaSyDzoEZVm8qLGy6Pog5Ob-xfh3Cv5YgwgrM';
     return $api;
 }
 
-/* Add CSS and JS to be handled by wp */
-add_action('wp_enqueue_scripts', 'clinic_resources');
 
+/**
+ * Hooks and scripts
+ */
+
+/* Add CSS and JS to be handled by Wordpress */
+add_action('wp_enqueue_scripts', 'clinic_resources');
 /* function to load CSS and JavaScript */
 add_action('after_setup_theme', 'clinic_features');
-
 /* hooking custom queries to wp */
 add_action('pre_get_posts', 'clinic_custom_queries');
-
 /* Google maps API key */
 add_filter('acf/fields/google_map/api', 'acf_google_maps_API_key');
-
 
 ?>
