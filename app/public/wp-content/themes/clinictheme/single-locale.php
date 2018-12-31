96aeb030f938ab0af07778391e92b52ad7343bbe
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-31 13:15:16 +0100

    added gMap to individual locale pages

--- app/public/wp-content/themes/clinictheme/single-locale.php
+++ app/public/wp-content/themes/clinictheme/single-locale.php
@@ -4,18 +4,30 @@ get_header();
 page_banner();
 
 while (have_posts()) {
-    the_post(); ?>
+    the_post(); 
+?>
 
-  <div class="container container--narrow page-section">
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('locale'); ?> ">
-          <i class="fa fa-home" aria-hidden="true"></i> All locales</a>
-        <span class="metabox__main">
-          <?php the_title(); ?> </span></p>
-    </div>
+<div class="container container--narrow page-section">
+  <div class="metabox metabox--position-up metabox--with-home-link">
+    <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('locale'); ?> ">
+    <i class="fa fa-home" aria-hidden="true"></i> All locales</a>
+    <span class="metabox__main">
+    <?php the_title(); ?> </span></p>
+  </div>
 
   <div class="generic_content">
     <?php the_content(); ?>
+  </div>
+
+  <div class="acf-map">
+    <?php
+      $map_location = get_field('map_location'); ?>
+      <div class="marker" 
+        data-lat="<?php echo $map_location['lat'] ?>" 
+        data-lng="<?php echo $map_location['lng'] ?>">
+        <h3><a><?php the_title(); ?></a></h3>
+        <?php echo $map_location['address']; ?>
+      </div>
   </div>
 
   <?php 
@@ -51,8 +63,7 @@ while (have_posts()) {
     echo '</ul>';
   } wp_reset_postdata(); ?>
     
-
-<?php
+  <?php
   $localeEvents = new WP_Query(array( /* Custom WP_Query for events displayed below page content */
     'posts_per_page' => 2,
     'post_type' => 'event',
