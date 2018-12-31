7df0d6a891f3373f5cd675846b6b875140c239ee
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-31 13:50:59 +0100

    now shows thumbnails for every locale on main page

--- app/public/wp-content/themes/clinictheme/archive-locale.php
+++ app/public/wp-content/themes/clinictheme/archive-locale.php
@@ -22,8 +22,24 @@
         <?php echo $map_location['address']; ?>
       </div>
     <?php }
-    echo paginate_links(); ?>
+    echo paginate_links();
+    wp_reset_postdata(); ?>
   </div>
+
+  <?php
+  echo '<hr class="section-break"><h2 class="headline headline--medium">Our locales</h2>';
+  echo '<ul class="employee-cards">';
+  while (have_posts()) {
+    the_post(); ?>
+
+    <li class="employee-card__list-item">
+    <a class="employee-card" href="<?php the_permalink(); ?>">
+      <img class="employee-card__image" src="<?php the_post_thumbnail_url($size="employee-landscape"); ?>">
+      <span class="employee-card__name"><?php the_title(); ?></span></a>
+    </li>
+
+  <?php } ?>
+
 </div>
 
 <?php
