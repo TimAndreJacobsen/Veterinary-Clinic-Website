1055814551dead87c3cb6187de9ffdba59a7ea1d
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-12-31 12:06:46 +0100

    changed design to Map layout with markers

--- app/public/wp-content/themes/clinictheme/archive-locale.php
+++ app/public/wp-content/themes/clinictheme/archive-locale.php
@@ -1,26 +1,27 @@
 <?php
-    get_header();
-    page_banner(array(
-      'title' => 'Locales',
-      'subtitle' => 'All services in one convenient location',
-      'image' => get_theme_file_uri('/images/countryside_buildings.jpg')
-    ));
+  get_header();
+  page_banner(array(
+    'title' => 'Locales',
+    'subtitle' => 'All services in one convenient location',
+    'image' => get_theme_file_uri('/images/countryside_buildings.jpg')
+  ));
 ?>
 
 <br><br>
-
 <div class="container container--narrow page section">
-  <ul class="link-list min-list">
+
+  <div class="acf-map">
     <?php
-  while (have_posts()) {
-      the_post(); ?>
-    <li><a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?></a></li>
-    <?php
-  }
-    echo paginate_links();
-?>
-  </ul>
+    while (have_posts()) {
+      the_post();
+      $map_location = get_field('map_location'); ?>
+      <div class="marker" 
+        data-lat="<?php echo $map_location['lat'] ?>" 
+        data-lng="<?php echo $map_location['lng'] ?>">
+      </div>
+    <?php }
+    echo paginate_links(); ?>
+  </div>
 </div>
 
 <?php
