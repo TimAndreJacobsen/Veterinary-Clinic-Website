5a6af0325f41a46b626aeb384d81692fa9489960
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-03 09:52:37 +0100

    import LiveSearch.js

--- app/public/wp-content/themes/clinictheme/js/scripts.js
+++ app/public/wp-content/themes/clinictheme/js/scripts.js
@@ -6,8 +6,11 @@ import slick from 'slick-carousel';
 import MobileMenu from './modules/MobileMenu';
 import HeroSlider from './modules/HeroSlider';
 import GoogleMap from './modules/GoogleMap';
+import LiveSeart from './modules/LiveSearch';
+import LiveSearch from './modules/LiveSearch';
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu();
 var heroSlider = new HeroSlider();
 var googleMap = new GoogleMap();
+var liveSearch = new LiveSearch();
