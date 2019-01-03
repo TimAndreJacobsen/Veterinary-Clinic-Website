4ebb8573c5071ad4e115ebed033da094e2daa630
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-03 10:10:20 +0100

    add "esc" to close overlay, "s" to open

--- app/public/wp-content/themes/clinictheme/js/modules/LiveSearch.js
+++ app/public/wp-content/themes/clinictheme/js/modules/LiveSearch.js
@@ -13,18 +13,32 @@ class LiveSearch {
     events(){
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
-        
+        $(document).on("keyup", this.keyPressHandler.bind(this));
     }
 
     // Functions/Methods
     openOverlay(){
         this.searchOverlay.addClass("search-overlay--active")
+        $("body").addClass("body-no-scroll");
     }
 
     closeOverlay(){
         this.searchOverlay.removeClass("search-overlay--active")
+        $("body").removeClass("body-no-scroll");
     }
-    
+
+    keyPressHandler(e){
+        console.log(e.keyCode)
+        if(e.key === "Escape") { // Escape pressed - close overlay
+            this.searchOverlay.removeClass("search-overlay--active")
+            $("body").removeClass("body-no-scroll");
+        }
+        if(e.keyCode == 83) { // Escape pressed - close overlay
+            this.searchOverlay.addClass("search-overlay--active")
+            $("body").addClass("body-no-scroll");
+        }
+    }
+
 }
 
 export default LiveSearch;
