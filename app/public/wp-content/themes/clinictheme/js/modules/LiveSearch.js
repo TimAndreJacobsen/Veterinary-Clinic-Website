840c10f49a2f3dc378b520bb30a7c87eb6ffa46d
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-04 09:55:23 +0100

    add relative url in search

--- app/public/wp-content/themes/clinictheme/js/modules/LiveSearch.js
+++ app/public/wp-content/themes/clinictheme/js/modules/LiveSearch.js
@@ -74,15 +74,15 @@ class LiveSearch {
     }
 
     getResults(){
-        $.getJSON('http://localhost:3000/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+        $.getJSON(clinic_data.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
             this.resultsDiv.html(`
-            <h2 class="search-overlay__section-title">Search Result</h2>
-            <ul class="link-list min-list">
-            ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-            </ul>
-          `);
+                <h2 class="search-overlay__section-title">Search Result</h2>
+                ${posts.length ? '<ul class="link-list min-list">' : '<p>No results matches the search</p>'}
+                ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+                ${posts.length ? '</ul>' : ''}
+            `);
         });
-        //this.isLoadingVisible = false;
+        this.isLoadingVisible = false;
     }
 
 }
