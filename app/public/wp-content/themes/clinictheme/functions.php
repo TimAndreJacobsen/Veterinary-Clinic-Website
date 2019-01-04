93d300017eebdca792910176ee8d974244784b3e
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-04 09:54:27 +0100

    add root _url data for use in REST/JSON search

--- app/public/wp-content/themes/clinictheme/functions.php
+++ app/public/wp-content/themes/clinictheme/functions.php
@@ -46,6 +46,9 @@ function clinic_resources(){
     /* JavaScript */
     wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDzoEZVm8qLGy6Pog5Ob-xfh3Cv5YgwgrM', NULL, '1.0', true);
     wp_enqueue_script('clinic_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+    wp_localize_script('clinic_js', 'clinic_data', array(
+        'root_url' => get_site_url()
+    ));
     /* CSS */
     wp_enqueue_style('font_google_roboto', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
