2d3086863f7f3c7a1bf5de22ee03d62d03b406c7
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-09 15:27:31 +0100

    Structured JSON output, categorized

--- app/public/wp-content/themes/clinictheme/includes/search-route.php
+++ app/public/wp-content/themes/clinictheme/includes/search-route.php
@@ -10,23 +10,69 @@ function clinic_register_search(){
     )); 
 }
 
+/**
+ * Custom REST api search result
+ * @param data search term input
+ * @return JSON_array structured JSON data
+ * 
+ * returns a JSON data structure and populates it by categories
+ */
 function clinic_search_results($data){
-    $employees = new WP_Query(array(
-        'post_type' => 'employee',
+    $main_query = new WP_Query(array(
+        'post_type' => array('post', 'page', 'employee', 'event', 'locale'), // post_type(s) to match against
         's' => sanitize_text_field($data['term']) //sanitize_text_field() = WP function to sanitize user input, prevent SQL injection.
     ));
 
-    $employee_result = array();
+    // create empty array structure
+    $query_results = array(
+        'posts_pages' => array(),
+        'employees' => array(),
+        'locales' => array(),
+        'events' => array(),
+        'treatments' => array()
+    );
 
-    while($employees->have_posts()){
-        $employees->the_post();
-        array_push($employee_result, array(
-            'title' => get_the_title(),
-            'link' => get_the_permalink()
-        ));
+    // populating JSON data structure by array categories
+    while($main_query->have_posts()){
+        $main_query->the_post();
+
+        // post_type: post and page
+        if(get_post_type() == 'post' OR get_post_type() == 'page') {
+            array_push($query_results['posts_pages'], array(
+                'title' => get_the_title(),
+                'link' => get_the_permalink()
+            ));
+        }
+        // post_type: employee
+        if(get_post_type() == 'employee') {
+            array_push($query_results['employees'], array(
+                'title' => get_the_title(),
+                'link' => get_the_permalink()
+            ));
+        }        
+        // post_type: locales
+        if(get_post_type() == 'locales') {
+            array_push($query_results['locales'], array(
+                'title' => get_the_title(),
+                'link' => get_the_permalink()
+            ));
+        }        
+        // post_type: event
+        if(get_post_type() == 'event') {
+            array_push($query_results['events'], array(
+                'title' => get_the_title(),
+                'link' => get_the_permalink()
+            ));
+        }        
+        // post_type: treatment
+        if(get_post_type() == 'treatment') {
+            array_push($query_results['treatments'], array(
+                'title' => get_the_title(),
+                'link' => get_the_permalink()
+            ));
+        }        
     }
-
-    return $employee_result;
+    return $query_results;
 }
 
 
