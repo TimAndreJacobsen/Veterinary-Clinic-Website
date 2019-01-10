c41aca641610f6558a28aacabcc98f07a5abf47c
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-10 13:05:08 +0100

    Search page result for non-JavaScript search functionality

--- app/public/wp-content/themes/clinictheme/search.php
+++ app/public/wp-content/themes/clinictheme/search.php
@@ -4,30 +4,20 @@
  * This page powers slug yoursite.com/s={searchterm}
  * 
  */
-
   get_header();
   page_banner(array(
     'title' => 'Search Results: &ldquo;' . esc_html(get_search_query(false)) . '&rdquo;', // esc_html - escape(sanitize) search term input, for displaying as HTML
     'subtitle' => 'For more features enable JavaScript to use full search functionality',
     'image' => get_theme_file_uri('/images/5_dogs.jpg')
   ));
-
 ?>
 
 <div class="container container--narrow page-section">
   <?php
   while (have_posts()) {
-      the_post(); 
-      
+      the_post();
       get_template_part('template-parts/content', get_post_type());
-
-      
-      
-      
       ?>
-
-
-
   <?php
   }
     echo paginate_links();
