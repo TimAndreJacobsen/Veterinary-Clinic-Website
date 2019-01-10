effc200102b33bb39f074657e16bbd485be08695
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-10 08:24:41 +0100

    connected search button to fallback non-JS search functionality /search

--- app/public/wp-content/themes/clinictheme/header.php
+++ app/public/wp-content/themes/clinictheme/header.php
@@ -10,8 +10,8 @@
 <body <?php body_class(); ?> >
   <header class="site-header">
     <div class="container">
-      <h1 class="clinic-logo-text float-left"><a href=" <?php echo esc_url(site_url()); ?> ">The <strong>Clinic</strong></a></h1>
-      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+      <h1 class="clinic-logo-text float-left"><a href="<?php echo esc_url(site_url()); ?>">The <strong>Clinic</strong></a></h1>
+      <a href="<?php esc_url(site_url('/search')); ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
         <nav class="main-navigation">
@@ -25,7 +25,7 @@
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
           <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
-          <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          <a href="<?php echo esc_url(site_url('/search')); ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
         </div>
       </div>
     </div>
