1de15d9bd0ef73b01c2726ba88ae718c797fc5b3
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-10 13:04:32 +0100

    Template part locale created

---
+++ app/public/wp-content/themes/clinictheme/template-parts/content-locale.php
@@ -0,0 +1,6 @@
+
+<li class="employee-card__list-item">
+    <a class="employee-card" href="<?php the_permalink(); ?>">
+    <img class="employee-card__image" src="<?php the_post_thumbnail_url($size="employee-landscape"); ?>">
+    <span class="employee-card__name"><?php the_title(); ?></span></a>
+</li>
