698e7a6a8b08eb8777e7821c4ef5b4f6483a54ee
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-11 13:37:36 +0100

    indentation

--- app/public/wp-content/themes/clinictheme/archive-locale.php
+++ app/public/wp-content/themes/clinictheme/archive-locale.php
@@ -33,7 +33,7 @@
     the_post(); ?>
 
     <li class="employee-card__list-item">
-    <a class="employee-card" href="<?php the_permalink(); ?>">
+      <a class="employee-card" href="<?php the_permalink(); ?>">
       <img class="employee-card__image" src="<?php the_post_thumbnail_url($size="employee-landscape"); ?>">
       <span class="employee-card__name"><?php the_title(); ?></span></a>
     </li>
--- app/public/wp-content/themes/clinictheme/template-parts/content-locale.php
+++ app/public/wp-content/themes/clinictheme/template-parts/content-locale.php
@@ -1,7 +1,7 @@
 <div class="post-item">
-<li class="employee-card__list-item">
-    <a class="employee-card" href="<?php the_permalink(); ?>">
-    <img class="employee-card__image" src="<?php the_post_thumbnail_url($size="employee-landscape"); ?>">
-    <span class="employee-card__name"><?php the_title(); ?></span></a>
-</li>
+    <li class="employee-card__list-item">
+        <a class="employee-card" href="<?php the_permalink(); ?>">
+        <img class="employee-card__image" src="<?php the_post_thumbnail_url($size="employee-landscape"); ?>">
+        <span class="employee-card__name"><?php the_title(); ?></span></a>
+    </li>
 </div>
