7d27649b6e5e8662772d2478a99951876051e5f1
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-11 16:27:59 +0100

    conditional, shows only for logged in user

--- app/public/wp-content/themes/clinictheme/page-my-pets.php
+++ app/public/wp-content/themes/clinictheme/page-my-pets.php
@@ -1,9 +1,13 @@
 <?php
-  get_header();
-  while (have_posts()) {
-      the_post(); 
-      page_banner();
-      ?>
+if(!is_user_logged_in()){
+    wp_redirect(esc_url(site_url('/')));
+    exit;
+}
+get_header();
+while (have_posts()) {
+    the_post(); 
+    page_banner();
+?>
 
 <div class="container container--narrow page-section">
 content goes here!
