7730dfaddaff3f3e8bff26df7a9c123991e3676e
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-13 10:54:17 +0100

    basic page for my-pets

--- app/public/wp-content/themes/clinictheme/page-my-pets.php
+++ app/public/wp-content/themes/clinictheme/page-my-pets.php
@@ -1,19 +1,38 @@
 <?php
-if(!is_user_logged_in()){
+if(!is_user_logged_in()){ // If not logged in, boot to front page.
     wp_redirect(esc_url(site_url('/')));
     exit;
 }
 get_header();
 while (have_posts()) {
     the_post(); 
-    page_banner();
+    page_banner(array(
+        'subtitle' => 'Personal pet information'
+    ));
 ?>
 
 <div class="container container--narrow page-section">
-content goes here!
+    <ul class="min-list link-list" id="my-notes">
+        <?php
+            $user_pets = new WP_query(array(
+                'post_type' => 'pet',
+                'post_per_page' => -1, //Display ALL
+                 'author' => get_current_user_id()
+            ));
+
+            while ($user_pets->have_posts()) {
+                $user_pets->the_post(); ?>
+                <li>
+                    <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+                    <textarea class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+                </li>
+
+
+            <?php }
+        ?>
+    </ul>
 </div>
 
-<?php
-  }
+<?php }
   get_footer();
 ?>
