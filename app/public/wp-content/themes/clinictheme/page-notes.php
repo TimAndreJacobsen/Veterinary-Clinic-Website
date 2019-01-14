be9f069256f870b65857404e7425be9caae71424
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 10:07:52 +0100

    added redirect for non-logged in users

--- app/public/wp-content/themes/clinictheme/page-notes.php
+++ app/public/wp-content/themes/clinictheme/page-notes.php
@@ -1,9 +1,13 @@
 <?php
-  get_header();
-  while (have_posts()) {
-      the_post(); 
-      page_banner();
-      ?>
+if(!is_user_logged_in()){ // If not logged in, boot to front page.
+    wp_redirect(esc_url(site_url('/')));
+    exit;
+}
+get_header();
+while (have_posts()) {
+    the_post(); 
+    page_banner();
+    ?>
 
 <div class="container container--narrow page-section">
 
