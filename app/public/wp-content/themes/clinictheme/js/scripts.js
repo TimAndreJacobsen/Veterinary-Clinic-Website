7115a82bde5917bdf923fcdd45a6d477a3a7f70f
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 11:24:09 +0100

    import new JS file MyNotes.js

--- app/public/wp-content/themes/clinictheme/js/scripts.js
+++ app/public/wp-content/themes/clinictheme/js/scripts.js
@@ -6,11 +6,12 @@ import slick from 'slick-carousel';
 import MobileMenu from './modules/MobileMenu';
 import HeroSlider from './modules/HeroSlider';
 import GoogleMap from './modules/GoogleMap';
-import LiveSeart from './modules/LiveSearch';
 import LiveSearch from './modules/LiveSearch';
+import MyNotes from './modules/MyNotes';
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu();
 var heroSlider = new HeroSlider();
 var googleMap = new GoogleMap();
 var liveSearch = new LiveSearch();
+var mynotes = new MyNotes();
