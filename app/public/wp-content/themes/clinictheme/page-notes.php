008f53a798963fca961730ce71915cdddcb53475
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 10:46:17 +0100

    basic skeleton for notes page

--- app/public/wp-content/themes/clinictheme/page-notes.php
+++ app/public/wp-content/themes/clinictheme/page-notes.php
@@ -4,22 +4,28 @@ if(!is_user_logged_in()){ // If not logged in, boot to front page.
     exit;
 }
 get_header();
-while (have_posts()) {
-    the_post(); 
-    page_banner();
-    ?>
+page_banner();
+?>
+
 
 <div class="container container--narrow page-section">
 
-
-  <div class="generic-content">
-  budy content here
-    <?php the_content(); ?>
-  </div>
+  <ul class="min-list link-list" id="my-notes">
+    <?php
+      $user_notes = new WP_Query(array(
+        'post_type' => 'note',
+        'posts_per_page' => -1,
+        'author' => get_current_user_id()
+      ));
+      
+    
+    ?>
+    
+  
+  </ul>
 
 </div>
 
 <?php
-  }
   get_footer();
 ?>
