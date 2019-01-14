ab9de5aa7af7a6fc114f9192a4b0ed18d8f947d8
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 11:45:26 +0100

    Add nonce to DELETE request to prove login status

--- app/public/wp-content/themes/clinictheme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/clinictheme/js/modules/MyNotes.js
@@ -12,8 +12,11 @@ class MyNotes {
     // Functions / Methods
     deleteNote() {
         $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('X-WP-Nonce', clinic_data.nonce);
+            },
             url: clinic_data.root_url + '/wp-json/wp/v2/note/205',
-            type: 'DELETE',
+            type: 'DELETE', 
             success: (response)=> {
                 console.log("Delete successfull");
                 console.log(response)
