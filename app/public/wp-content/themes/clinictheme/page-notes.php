1424c94be284abc280f9c82207c2eaa57909c632
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 12:58:47 +0100

    create new note functionality added

--- app/public/wp-content/themes/clinictheme/page-notes.php
+++ app/public/wp-content/themes/clinictheme/page-notes.php
@@ -9,6 +9,13 @@ page_banner();
 
 <div class="container container--narrow page-section">
 
+  <div class="create-note">
+    <h2 class="headline headline--medium">Create new note</h2>
+    <input placeholder="note title" class="new-note-title">
+    <textarea placeholder="..." class="new-note-body"></textarea>
+    <span class="submit-note">Create Note</span>
+  </div>
+
   <ul class="min-list link-list" id="my-notes">
     <?php
       $user_notes = new WP_Query(array(
