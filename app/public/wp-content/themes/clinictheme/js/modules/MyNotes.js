ed55d63856620cbe652a6a3ba4c51521c774debc
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 12:03:02 +0100

    delete note implemented. sends DEL request and uses slideUp() to fade note away.

--- app/public/wp-content/themes/clinictheme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/clinictheme/js/modules/MyNotes.js
@@ -10,20 +10,22 @@ class MyNotes {
     }
 
     // Functions / Methods
-    deleteNote() {
+    deleteNote(e) {
+        var thisNote = $(e.target).parents("li");
         $.ajax({
             beforeSend: (xhr) => {
                 xhr.setRequestHeader('X-WP-Nonce', clinic_data.nonce);
             },
-            url: clinic_data.root_url + '/wp-json/wp/v2/note/205',
+            url: clinic_data.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
             type: 'DELETE', 
             success: (response)=> {
+                thisNote.slideUp();
                 console.log("Delete successfull");
-                console.log(response)
+                console.log(response);
             },
             error: (response) => {
                 console.log("Delete ERROR");
-                console.log(response)
+                console.log(response);
             }
         })
     }
