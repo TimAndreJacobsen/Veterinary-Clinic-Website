b67e26fc37a861c40643b8c3a53376c799fe3a58
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 12:03:45 +0100

    add readonly attr by default.

--- app/public/wp-content/themes/clinictheme/page-notes.php
+++ app/public/wp-content/themes/clinictheme/page-notes.php
@@ -18,12 +18,14 @@ page_banner();
       ));
       while($user_notes->have_posts()) {
         $user_notes->the_post(); ?>
-        <li>
-          <input class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+
+        <li data-id="<?php the_ID(); ?>">
+          <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-          <textarea class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+          <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
         </li>
+
       <?php } ?>
   </ul>
 
