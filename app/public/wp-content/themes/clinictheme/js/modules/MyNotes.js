331dacb8eabd44758abf3db36feeb9d93575f457
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-14 12:12:50 +0100

    when in edit-mode, display save-button

--- app/public/wp-content/themes/clinictheme/js/modules/MyNotes.js
+++ app/public/wp-content/themes/clinictheme/js/modules/MyNotes.js
@@ -34,6 +34,7 @@ class MyNotes {
     editNote(e) {
         var thisNote = $(e.target).parents("li");
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
+        thisNote.find(".update-note").addClass("update-note--visible");
     }
 
 
