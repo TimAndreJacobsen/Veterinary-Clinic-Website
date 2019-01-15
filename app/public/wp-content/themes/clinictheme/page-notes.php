432a62c7d9cd6b1722e1a1d9fbde0db324b2b422
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-15 11:00:07 +0100

    updated esc_method to a more appropriate esc_textarea()

--- app/public/wp-content/themes/clinictheme/page-notes.php
+++ app/public/wp-content/themes/clinictheme/page-notes.php
@@ -30,7 +30,7 @@ page_banner();
           <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); /* This monstrosity: str_replace(<str_to_match>, <str_to_replace_match>, <full string>) | esc_attr() escapes str from server(security reasons) */ ?>">
           <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
           <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-          <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+          <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
           <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
         </li>
 
