1fccb6abea0920b9d6b187ee1fc004c7a211b196
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-16 17:55:16 +0100

    REST fields for 'pet': name, age, breed, pet_count

--- app/public/wp-content/themes/clinictheme/functions.php
+++ app/public/wp-content/themes/clinictheme/functions.php
@@ -126,6 +126,19 @@ function clinic_custom_rest() {
     register_rest_field('note', 'user_note_count', array(
         'get_callback' => function(){return count_user_posts(get_current_user_id(), 'note');}
     ));
+    
+    register_rest_field('pet', 'user_pet_count', array(
+        'get_callback' => function(){return count_user_posts(get_current_user_id(), 'pet');}
+    ));
+    register_rest_field('pet', 'name', array(
+        'get_callback' => function(){ return 'pet_name_here' ;}
+    ));
+    register_rest_field('pet', 'age', array(
+        'get_callback' => function(){return 'pet_age_here' ;}
+    ));
+    register_rest_field('pet', 'breed', array(
+        'get_callback' => function(){return 'pet_breed_here' ;}
+    ));
 }
 
 /**
