33069af238eea67f016abc41f944ca0c312bada6
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-16 17:57:01 +0100

    initial commit - basic page for mypets

--- app/public/wp-content/themes/clinictheme/page-my-pets.php
+++ app/public/wp-content/themes/clinictheme/page-my-pets.php
@@ -4,42 +4,44 @@ if(!is_user_logged_in()){ // If not logged in, boot to front page.
     exit;
 }
 get_header();
-while (have_posts()) {
-    the_post(); 
-    page_banner(array(
-        'subtitle' => 'Personal pet information'
-    ));
+page_banner();
 ?>
 
 <div class="container container--narrow page-section">
-    <ul class="min-list link-list" id="my-notes">
-        <?php
-            $user_pets = new WP_query(array(
-                'post_type' => 'pet',
-                'post_per_page' => -1, //Display ALL
-                 'author' => get_current_user_id()
-            ));
 
-            while ($user_pets->have_posts()) {
-                $user_pets->the_post(); ?>
-                <div class="generic_content">
-                    <div class="row group"> 
-                        <div class="one-third"> <?php /* Pet thumbnail */
-                            the_post_thumbnail('employee-portrait'); ?>
-                        </div>
+  <div class="create-note">
+    <h2 class="headline headline--medium">Register new pet</h2>
+    <input id="name" placeholder="Name of pet" class="new-note-title">
+    <input id="breed" placeholder="Type of animal: dog, cat, squirrel etc" class="new-note-title">
+    <input id="age" placeholder="Age" class="new-note-title">
+    <textarea placeholder="Other relevant information" class="new-note-body"></textarea>
+    <span class="submit-note">Register pet</span>
+    <span class="note-limit-message">Per user pet limit is 10 pets, please delete a pet to free up space.</span>
+  </div>
 
-                        <div class="two-thirds"><?php /* Pet Bio text */ 
-                            the_content(); ?>
-                        </div>
-                    </div>
-                </div> <br>
+  <ul class="min-list link-list" id="my-notes">
+    <?php
+      $user_pets = new WP_Query(array(
+        'post_type' => 'pet',
+        'posts_per_page' => -1,
+        'author' => get_current_user_id()
+      ));
+      while($user_pets->have_posts()) {
+        $user_pets->the_post(); ?>
 
+        <li data-id="<?php the_ID(); ?>">
+          <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); /* This monstrosity: str_replace(<str_to_match>, <str_to_replace_match>, <full string>) | esc_attr() escapes str from server(security reasons) */ ?>">
+          <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+          <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+          <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
+          <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
+        </li>
 
-            <?php }
-        ?>
-    </ul>
+      <?php } ?>
+  </ul>
+
 </div>
 
-<?php }
+<?php
   get_footer();
 ?>
