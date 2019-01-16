dbbf36e26fe1aad4f58f4a00d3f91fab0beed786
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-16 17:56:25 +0100

    importing and instantiating MyPets

--- app/public/wp-content/themes/clinictheme/js/scripts.js
+++ app/public/wp-content/themes/clinictheme/js/scripts.js
@@ -8,6 +8,7 @@ import HeroSlider from './modules/HeroSlider';
 import GoogleMap from './modules/GoogleMap';
 import LiveSearch from './modules/LiveSearch';
 import MyNotes from './modules/MyNotes';
+import MyPets from './modules/MyPets';
 
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu();
@@ -15,3 +16,4 @@ var heroSlider = new HeroSlider();
 var googleMap = new GoogleMap();
 var liveSearch = new LiveSearch();
 var mynotes = new MyNotes();
+var mypets = new MyPets();
