c4eafb888a896e50759c2c5e8ee6ae6ecf00456f
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-20 13:50:06 +0100

    finally added a title to the head. Long overdue

--- app/public/wp-content/themes/clinictheme/header.php
+++ app/public/wp-content/themes/clinictheme/header.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?> ">
   <meta name="viewport" content="width=device-width" , initial-scale=1>
+  <title>The Clinic - Custom theme</title>
   <?php wp_head(); ?>
 </head>
 
