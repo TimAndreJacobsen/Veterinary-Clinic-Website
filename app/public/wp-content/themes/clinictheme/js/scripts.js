4f706c36c2d1f8da7e3b69bf053bb39a0ecce03a
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-03-28 12:35:41 +0100

    added instantiation and import for class MyPets.js

--- app/public/wp-content/themes/clinictheme/js/scripts.js
+++ app/public/wp-content/themes/clinictheme/js/scripts.js
@@ -2,16 +2,18 @@
 import $ from 'jquery';
 import slick from 'slick-carousel';
 
-// Our modules / classes
+// Modules & Classes
 import MobileMenu from './modules/MobileMenu';
 import HeroSlider from './modules/HeroSlider';
 import GoogleMap from './modules/GoogleMap';
 import LiveSearch from './modules/LiveSearch';
 import MyNotes from './modules/MyNotes';
+import MyPets from './modules/MyPets';
 
-// Instantiate a new object using our modules/classes
+// Instantiate a new modules/classes
 var mobileMenu = new MobileMenu();
 var heroSlider = new HeroSlider();
 var googleMap = new GoogleMap();
 var liveSearch = new LiveSearch();
-var mynotes = new MyNotes();
+var myNotes = new MyNotes();
+var myPets = new MyPets();
